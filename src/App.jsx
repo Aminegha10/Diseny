3a785fa7b1f59c27c735f418a88660ff962505b0
amine <aminogha@gmail.com>
2025-03-21 00:38:10 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+import HeroSection from "./HeroSection";
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+        herosection
+    </div>
+  )
+}
+
+export default HeroSection
